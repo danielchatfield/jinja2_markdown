f07bdc5e5d471c3e5c5f1b274e47dae0651de80c
Daniel Chatfield <chatfielddaniel@gmail.com>
2014-01-03 20:23:17 +0000

    Fix typo

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 	name='jinja2_markdown',
 	author='Daniel Chatfield',
 	author_email='chatfielddaniel@gmail.com',
-	version='0.0.1',
+	version='0.0.2',
 	ur='http://github.com/danielchatfield/jinja2_markdown_extension',
 	py_modules=['jinja2_markdown'],
 	description='A jinja2 extension that adds a '
@@ -18,7 +18,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
 	],
-	install_requries=[
+	install_requires=[
 		'Markdown>=2.3.1',
 		'Jinja2>=2.4'
 	]
