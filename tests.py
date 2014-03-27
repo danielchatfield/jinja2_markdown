f14b00f81855f2c7e2e051ef06ea71d10b7cc3a1
Daniel Chatfield <chatfielddaniel@gmail.com>
2014-03-27 21:21:44 +0000

    Add unit tests

---
+++ tests.py
@@ -0,0 +1,26 @@
+import os
+import unittest
+from jinja2 import Environment, Template, FileSystemLoader
+from jinja2_markdown import MarkdownExtension
+
+
+class Jinja2MarkdownTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.env = Environment(loader=FileSystemLoader(os.path.dirname(__file__)),extensions=['jinja2_markdown.MarkdownExtension'])
+
+    def test_parsing(self):
+        input = ("{% markdown %}"
+                 "Regular text"
+                 "# Heading"
+                 "{% endmarkdown %}")
+
+        output = ("<p>Regular text</p>\n"
+                  "<h1>Heading</h1>")
+
+        template = self.env.get_template('test_input.html')
+        self.assertEqual(template.render(), output)
+
+
+if __name__ == '__main__':
+    unittest.main()
